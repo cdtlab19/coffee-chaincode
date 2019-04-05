8e238bbbddf9a6fc674e0dbf4b2e6b754b435348
Victor Franco <victorfrancovl@gmail.com>
2019-04-05 15:46:45 -0300

    Added test stubs

---
+++ chaincode/user_test.go
@@ -0,0 +1,12 @@
+package chaincode_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/cdtlab19/coffee-chaincode/chaincode"
+)
+
+var _ = Describe("User", func() {
+
+})
---
+++ model/user_test.go
@@ -0,0 +1,12 @@
+package model_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/cdtlab19/coffee-chaincode/model"
+)
+
+var _ = Describe("User", func() {
+
+})
