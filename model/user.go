043a6d3a8282aa13efe4de54505855efab55c1d2
Cleanderson Lins <cleandersonlins@gmail.com>
2019-04-05 22:05:28 -0300

    Added some user's chaincode unit tests

---
+++ model/user.go
@@ -0,0 +1,60 @@
+package model
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// UserPrefix is the prefix used in User's ID
+const UserPrefix = "USER"
+
+// UserDocType is the DocType use in model
+const UserDocType = "USER"
+
+// UserKey returns the User state key for a given user id
+func UserKey(id string) string {
+	return fmt.Sprintf("%s-%s", UserPrefix, id)
+}
+
+// User defines a basic model for an user
+type User struct {
+	DocType string `json:"docType"`
+	ID      string `json:"id"`
+	Name    string `json:"name"`
+}
+
+// NewUser creates an user
+func NewUser(id string, name string) *User {
+	return &User{
+		DocType: UserDocType,
+		ID:      id,
+		Name:    name,
+	}
+}
+
+// Key returns the User's state key
+func (u *User) Key() string {
+	return UserKey(u.ID)
+}
+
+// Valid verifies if an User is valid
+func (u *User) Valid() error {
+	if u.DocType != UserDocType {
+		return fmt.Errorf("user docType not set to '%s'", UserDocType)
+	}
+	if u.ID == "" {
+		return fmt.Errorf("missing user ID")
+	}
+
+	if u.Name == "" {
+		return fmt.Errorf("missing user name")
+	}
+
+	return nil
+}
+
+// JSON encodes an user model as a JSON object
+func (u *User) JSON() []byte {
+	v, _ := json.Marshal(u)
+	return v
+}
---
+++ model/user_test.go
@@ -0,0 +1,56 @@
+package model_test
+
+import (
+	"encoding/json"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/cdtlab19/coffee-chaincode/model"
+)
+
+var _ = Describe("User", func() {
+	It("Should create a valid user", func() {
+		user := NewUser("id", "someone")
+		Expect(user.DocType).To(Equal(UserDocType))
+		Expect(user.ID).To(Equal("id"))
+		Expect(user.Name).To(Equal("someone"))
+		Expect(user.Key()).To(Equal(UserKey(user.ID)))
+
+		err := user.Valid()
+		Expect(err).NotTo(HaveOccurred())
+
+	})
+
+	It("Should have a valid ID", func() {
+		user := NewUser("", "someone")
+		err := user.Valid()
+		Expect(err).To(HaveOccurred())
+	})
+
+	It("Should have a valid name", func() {
+		user := NewUser("id", "")
+		err := user.Valid()
+		Expect(err).To(HaveOccurred())
+	})
+
+	It("Should have a valid name", func() {
+		user := NewUser("id", "someone")
+		user.DocType = ""
+		err := user.Valid()
+		Expect(err).To(HaveOccurred())
+	})
+
+	It("Should be encodable", func() {
+		user := NewUser("id", "someone")
+		jsonUser := user.JSON()
+
+		var raw map[string]string
+		Expect(json.Unmarshal(jsonUser, &raw)).NotTo(HaveOccurred())
+		Expect(raw).To(HaveKeyWithValue("id", user.ID))
+		Expect(raw).To(HaveKeyWithValue("name", user.Name))
+		Expect(raw).To(HaveKeyWithValue("docType", user.DocType))
+
+	})
+
+})
